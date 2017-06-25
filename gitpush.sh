3e2111b16c52edc07b192d70fc74d5535df1b9fe
root <root@federalgovopps.com>
2017-06-25 08:43:03 -0400

    Added new picks

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-/usr/bin/php /var/www/html/my-api.us/suggested-stock-picker/nyse.php
-/usr/bin/php /var/www/html/my-api.us/suggested-stock-picker/amex.php
-/usr/bin/php /var/www/html/my-api.us/suggested-stock-picker/nasdaq.php
-
 git add .
 git commit -m "Added new picks"
-git push origin master
+HOME=/home/root git push origin master
 
